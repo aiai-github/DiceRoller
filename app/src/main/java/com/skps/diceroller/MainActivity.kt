4cb2619657595bd3793091ec0b06d567b7e14f02
aiai-github <kazuki.aigoh@gmail.com>
2022-06-16 22:41:23 +0900

    Final Commit Final

--- app/src/main/java/com/skps/diceroller/MainActivity.kt
+++ app/src/main/java/com/skps/diceroller/MainActivity.kt
@@ -3,38 +3,48 @@ package com.skps.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var diceImage : ImageView
+    lateinit var diceImageSub : ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val rollButton: Button = findViewById(R.id.roll_button)
-        val countUpButton: Button = findViewById(R.id.count_up)
         rollButton.setOnClickListener { rollDice() }
-        countUpButton.setOnClickListener { countUp() }
+
+        diceImage = findViewById(R.id.dice_image)
+        diceImageSub = findViewById(R.id.dice_image_sub)
     }
     private fun rollDice() {
         // Toast.makeText(this, "button clicked",
         //    Toast.LENGTH_SHORT).show()
 
-        val randomInt = (1..6).random()
-        val resultText: TextView = findViewById(R.id.result_text)
-        resultText.text = randomInt.toString()
-    }
-    private fun countUp() {
-        val resultText: TextView = findViewById(R.id.result_text)
-        if (resultText.text.toString() == "Hello World!") {
-            resultText.text = "1"
-        } else {
-            var resultTextInt = Integer.parseInt(resultText.text.toString())
-
-            if (resultTextInt < 6) {
-                resultTextInt ++
-                resultText.text = resultTextInt.toString()
-            }
+        val drawableResource = when ((1..6).random()) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
         }
+        val drawableResourceSub = when ((1..6).random()) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+
+        diceImage.setImageResource(drawableResource)
+        diceImageSub.setImageResource(drawableResourceSub)
     }
 }
